5895f54726f6549f999ac2c92a983c4f41d180cc
rythmern02 <117756525+rythmern02@users.noreply.github.com>
2023-10-29 15:08:59 +0530

    Add files via upload

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'my-lime': 'rgb(212, 255, 42)',
+        'my-blue': 'rgb(4, 0, 93)',
+      },
+    },
+  },
+  plugins: [],
+}
